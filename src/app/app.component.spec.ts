0533a10ec58475cd7c7cbc3e9e78b209a90080b7
kzybul <krzysztof.zybul@hyand.com>
2025-05-07 09:40:38 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'efizjo-client' title`, () => {
+  it(`should have the 'efizjo_client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('efizjo-client');
+    expect(app.title).toEqual('efizjo_client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, efizjo-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, efizjo_client');
   });
 });
